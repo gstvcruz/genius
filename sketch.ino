373e8562931833facabbed67ac93ab2da38b58fe
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2024-04-04 13:12:30 -0300

    Adiciona solução final e correção de bugs

--- sketch.ino
+++ sketch.ino
@@ -1,26 +1,27 @@
-#define F_LEVEL 0
-#define LEVELS 100
-int level = F_LEVEL;
+#define RANDOM_SEED_PORT 11
 
 /*
 Max and min interval between blinks when showing LED sequence
 to the player. The interval gets progressively closer to MIN_INTER
-as the level goes up
+as the level goes up.
 */
 #define MAX_INTER 750
 #define MIN_INTER 250
 
-#define F_LED 7
-#define L_LED 10
-int sequence[LEVELS];  // Keeps track of LED sequence to be displayed
+#define FIRST_LVL 0
+#define LEVELS 100
+int level = FIRST_LVL;
 
-// Start button, first button and last button ports
-#define S_BTN 2
-#define F_BTN 3
-#define L_BTN 6
+#define FIRST_LED 7
+#define LAST_LED 10
+int LEDSequence[LEVELS];
+
+#define START_BTN 2
+#define FIRST_BTN 3
+#define LAST_BTN 6
 
 /*
-Keeps track of buttons states
+Keeps track of whether buttons are pressed or not
 
 0 = Start button
 1 = Blue button
@@ -28,91 +29,77 @@ Keeps track of buttons states
 3 = Red button
 4 = Green button
 */
-bool btnStates[5][2];
-#define C_READ 0 // Current value read: btnStates[i][C_READ]
-#define P_READ 1 // Previous value read: btnStates[i][P_READ]
+bool buttonsState[5];
 
-// Positions on btnStates: btnSates[Z_BTN_POS][i]
-#define S_BTN_POS 0
-#define B_BTN_POS 1
-#define Y_BTN_POS 2
-#define R_BTN_POS 3
-#define G_BTN_POS 4
-
-// Analogic port for seeding random LEDs into sequence
-#define SEED_PORT 11
-
-/* 
-Keeps track wether the game is running (true)
-or waiting for start button to be pressed (false)
-*/
-bool game_status = false;
+bool gameIsRunning = false;
 
 void setup() {
-  // Set buttons
-  for (int btn = S_BTN; btn <= L_BTN; btn++) {
+  for (int btn = START_BTN; btn <= LAST_BTN; btn++)
     pinMode(btn, INPUT_PULLUP);
-  }
-  // Set buttons states
-  clearButtonStates();
-  // Set LEDs
-  for (int led = F_LED; led <= L_LED; led++) {
+  clearButtonsState();
+  for (int led = FIRST_LED; led <= LAST_LED; led++)
     pinMode(led, OUTPUT);
-  }
-  randomSeed(analogRead(SEED_PORT));
+  randomSeed(analogRead(RANDOM_SEED_PORT));
 }
 
 void loop() {
   // Wait for start button press
-  while (!game_status) {
-    btnStates[S_BTN_POS][C_READ] = digitalRead(S_BTN);
-    if (!btnStates[S_BTN_POS][C_READ] && btnStates[S_BTN_POS][P_READ]) {
-      game_status = true;
-      updateSequence(0);
-    }      
+  while (!gameIsRunning) {
+    if (buttonsState[0] = digitalRead(START_BTN)) continue;
+    gameIsRunning = true;
+    updateLEDSequence(0);
   }
 
-  // Display sequence
-  for (int i = F_LEVEL; i < LEVELS; i++) {
-    if (sequence[i] == 0) {
-      updateSequence(i);
+  // Display LED sequence
+  for (int i = FIRST_LVL; i < LEVELS; i++) {
+    if (LEDSequence[i] == 0) {
+      updateLEDSequence(i);
       break;
     }
-    blinkSingle(sequence[i]);
+    blinkSingle(LEDSequence[i]);
   }
-
-  // Player's turn
-  for (int i = F_LEVEL; i <= level; i++) {
-    // Which button should be pressed based on LED color in sequence
-    int btn = map(sequence[i], F_LED, L_LED, F_BTN, L_BTN);
-
-    int j = B_BTN_POS;
-    int k = F_BTN;
-    // Scan for button presses
-    while (true) {
-      btnStates[j][C_READ] = digitalRead(k);
-      if (!btnStates[j][C_READ] && btnStates[j][P_READ] && k == btn) {
+  
+  int i = FIRST_LVL;
+  int j = 0;
+  int k = START_BTN;
+  int btn = updateButtonToBePressed(i);
+  // Scan for button presses on player's turn
+  while (gameIsRunning) {
+    buttonsState[j] = digitalRead(k);
+    // If button being read was pressed
+    if (!buttonsState[j]) {
+      // It is the right button
+      if (k == btn) {
+        blinkSingle(LEDSequence[i]);
+        if (i < level) {
+          btn = updateButtonToBePressed(++i);
+          clearButtonsState();
+        } else break;
+      } 
+      // It is the wrong button
+      else {
+        gameIsRunning = false;
         break;
-      } else {
-        if (j < G_BTN_POS && k < L_BTN) {
-          j++;
-          k++;
-        } else {
-          j = B_BTN_POS;
-          k = F_BTN;
-        }
       }
+    }
+
+    // No button was pressed
+    if (j < 4 && k < LAST_BTN) {
+      j++;
+      k++;
+    } else {
+      j = 0;
+      k = START_BTN;
     }
   }
 
-  if (game_status) {
+  clearButtonsState();
+  if (gameIsRunning) {
     level++;
     blinkAll(1);
-    clearButtonStates();
   } else {
-    clearSequence();
-    clearButtonStates();
-    level = F_LEVEL;
+    clearLEDSequence();
+    level = FIRST_LVL;
     blinkAll(5);
   }
 }
@@ -120,38 +107,41 @@ void loop() {
 // Blink a single LED with the time between on and off based on game level
 void blinkSingle(int led) {
   digitalWrite(led, HIGH);
-  delay(map(level, F_LEVEL, LEVELS, MAX_INTER, MIN_INTER));
+  delay(map(level, FIRST_LVL, LEVELS, MAX_INTER, MIN_INTER));
   digitalWrite(led, LOW);
-  delay(map(level, F_LEVEL, LEVELS, MAX_INTER, MIN_INTER));
+  delay(map(level, FIRST_LVL, LEVELS, MAX_INTER, MIN_INTER));
 }
 
 // All LEDs blink simultaneously n times
 void blinkAll(int times) {
   while (times-- > 0) {
-    for (int led = F_LED; led <= L_LED; led++)
+    for (int led = FIRST_LED; led <= LAST_LED; led++)
       digitalWrite(led, HIGH);
-    delay(250);
-    for (int led = F_LED; led <= L_LED; led++)
+    delay(MIN_INTER);
+    for (int led = FIRST_LED; led <= LAST_LED; led++)
       digitalWrite(led, LOW);
-    delay(250);
+    delay(MIN_INTER);
   }
 }
 
-// Add a random LED to sequence in position i
-void updateSequence(int i) {
-  sequence[i] = random(F_LED, L_LED + 1);
+// Add a random LED to LEDSequence in position i
+void updateLEDSequence(int i) {
+  LEDSequence[i] = random(FIRST_LED, LAST_LED + 1);
+}
+
+// Determines which button should be pressed based on sequence's LED color
+int updateButtonToBePressed(int i) {
+  return map(LEDSequence[i], FIRST_LED, LAST_LED, FIRST_BTN, LAST_BTN);
 }
 
 // Clear LED sequence
-void clearSequence() {
-  for (int i = 0; sequence[i] > 0; i++)
-    sequence[i] = 0;
+void clearLEDSequence() {
+  for (int i = FIRST_LVL; LEDSequence[i] != 0; i++)
+    LEDSequence[i] = 0;
 }
 
-// Set all button states to currently and previously not pressed (true)
-void clearButtonStates() {
-  for (int i = 0; i < 5; i++) {
-    btnStates[i][C_READ] = true;
-    btnStates[i][P_READ] = true;
-  }
+// Set all buttons to not pressed
+void clearButtonsState() {
+  for (int btn = 0; btn < 4; btn++)
+    buttonsState[btn] = true;
 }
